40f062cd1b4a9f13398350359fd3388c1f0189d4
Damir Herman <damir@tag.bio>
2020-04-14 05:27:12 -0700

    Added return hints and provided more debugging logging

--- tagbiopy/regression/elastic_net.py
+++ tagbiopy/regression/elastic_net.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import logging
 import pandas as pd
 
 from sklearn.pipeline import Pipeline
@@ -7,8 +7,16 @@ from sklearn.impute import SimpleImputer
 from sklearn.model_selection import LeaveOneOut, cross_val_predict
 from sklearn.linear_model import ElasticNetCV
 
+logger = logging.getLogger(__name__)
 
-def create_pipeline(impute_missing_values=True, **kwargs):
+
+def create_pipeline(impute_missing_values=True, **kwargs) -> Pipeline:
+    """
+    Creates a Pipeline that always ends with ElasticNetCV estimator. The first step may be imputer.
+    :param impute_missing_values: bool, default True
+    :param kwargs: dict, if non-default elastic net cross validation parameters are desired
+    :return: Pipeline
+    """
     params = elastic_net_cross_validation_params(**kwargs)
 
     estimator = ElasticNetCV(**params)
@@ -22,8 +30,8 @@ def create_pipeline(impute_missing_values=True, **kwargs):
     return pipeline
 
 
-def elastic_net_cross_validation_params(**kwargs):
-    # li_ratio: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNetCV.html
+def elastic_net_cross_validation_params(**kwargs) -> dict:
+    # l1_ratio: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNetCV.html
     # The regressors X will be normalized before regression by subtracting the
     # mean and dividing by the l2-norm.
 
@@ -48,7 +56,7 @@ def elastic_net_cross_validation_params(**kwargs):
     }
 
 
-def elastic_net_cross_validation_worker(outcome, observations, verbose=0, **kwargs):
+def elastic_net_cross_validation_worker(outcome, observations, verbose=0, **kwargs) -> (pd.Series, dict):
     params = elastic_net_cross_validation_params(**kwargs)
 
     gscv = create_pipeline(**params)
@@ -65,19 +73,39 @@ def elastic_net_cross_validation_worker(outcome, observations, verbose=0, **kwar
     return predicted_outcome, params
 
 
-def set_plot_label(text, params, verbose=True):
-    import decimal
-    label = text
-    if verbose:
-        label += '\ntol: {:.1E}'.format(decimal.Decimal(params['tol']))
-        label += ', max_iter: {:.1E}'.format(decimal.Decimal(params['max_iter']))
-
-    return label
-
-
-def set_outcome_and_observations(df, outcome_column):
+def elastic_net_cross_validation(outcome, observations, **kwargs) -> (pd.Series, dict):
     """
 
+    :param outcome: pd.Series, observed outcome
+    :param observations: pd.DataFrame, predictors
+    :param kwargs: dict, optional args to be passed to elastic_net_cross_validation_params function to
+        tweak the elastic net default model params.
+    :return: 2-touple
+        predicted_outcome: pd.Series, predicted values
+        params: dict, what was used by elastic net cross validation
+    """
+    params = elastic_net_cross_validation_params(**kwargs)
+
+    gscv = create_pipeline(**params)
+    logger.debug('gscv: {}'.format(str(gscv)))
+
+    logger.debug('Starting cross_val_predict')
+    _data = cross_val_predict(estimator=gscv,
+                              X=observations,
+                              y=outcome,
+                              cv=LeaveOneOut(),
+                              n_jobs=params['n_jobs']
+                              )
+    logger.debug('Completed cross_val_predict')
+
+    predicted_outcome = pd.Series(data=_data, index=outcome.index, name='Predicted {}'.format(outcome.name))
+
+    return predicted_outcome, params
+
+
+def set_outcome_and_observations(df, outcome_column) -> (pd.Series, pd.DataFrame):
+    """
+    Separates the input dataframe into the outcome vector and a matrix of variables (predictors)
     :param df: pd.DataFrame, input dataframe
     :param outcome_column: str
     :return: 2-touple
@@ -86,24 +114,24 @@ def set_outcome_and_observations(df, outcome_column):
     """
     _df = df.copy()
     # Remove all rows that don't have outcome data
+    input_shape = _df.shape
+    logger.debug('Input df shape: {}'.format(str(input_shape)))
     _df = _df.dropna(subset=[outcome_column])
+    final_shape = _df.shape
+    if input_shape != final_shape:
+        logger.debug("After removing NaN's from the outcome, we have new shape: {}".format(str(final_shape)))
+
     outcome = _df[outcome_column]
     observations = _df[[v for v in _df.columns if v != outcome_column]]
 
     return outcome, observations
 
 
-def elastic_net_cross_validation(y, observations, **kwargs):
-    """
+def set_plot_label(text, params, verbose=True) -> str:
+    import decimal
+    label = text
+    if verbose:
+        label += '\ntol: {:.1E}'.format(decimal.Decimal(params['tol']))
+        label += ', max_iter: {:.1E}'.format(decimal.Decimal(params['max_iter']))
 
-    :param y: pd.Series, observed outcome
-    :param observations: pd.DataFrame, predictors
-    :param kwargs: dict, optional args to be passed to elastic_net_cross_validation_params function to
-        tweak the elastic net default model params.
-    :return: 2-touple
-        y_hat: pd.Series, predicted values
-        model_params: dict, what was used by elastic net cross validation
-    """
-    y_hat, model_params = elastic_net_cross_validation_worker(y, observations, **kwargs)
-
-    return y_hat, model_params
+    return label
