192af7be7e0e67147af24b2819804ab227421913
D <damir@tag.bio>
2023-03-17 15:07:16 -0700

    Introduce error, check git

--- src/tagbiopy/request.py
+++ src/tagbiopy/request.py
@@ -321,6 +321,6 @@ class QRequest(_Request):
         if background is not None:
             ret['script'].update({'background': background})
 
-        logger.debug(f'{self!r}: payload: {json.dumps(ret, indent=2, default=to_json)}')
+        logger.debug(f'{self!r}: payload: {json.dumps(ret, indent=2, default=_to_json)}')
 
         return ret
