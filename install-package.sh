95743256fd4f4da4a4b457dbabb7e046b86e4cb8
D <damir@tag.bio>
2023-06-27 15:22:36 -0700

    Use TAGBIO_PY env variables to install (see tagbio R)

--- install-package.sh
+++ install-package.sh
@@ -1,19 +1,15 @@
 #! /usr/bin/env sh
 set -ex
 
-SCRIPT_DIR="$(dirname $(realpath $0))"
-echo "**** 0=$0"
-echo "**** realpath=$(realpath $0)"
-echo "**** dirname realpath=$(dirname $(realpath $0) )"
-echo "**** SCRIPT_DIR=$SCRIPT_DIR"
 
-echo "*** Building tagbio python library in dev mode"
+echo "*** Building tagbio python library in dev mode in ${TAGBIO_PY}"
 
-conda env update -f ${SCRIPT_DIR}/environment.yml
+#conda env update -f ${SCRIPT_DIR}/environment.yml
 
 # Tue Jun 27 11:08:42 PDT 2023
 # To use tagbiopy as a package under pip, no dependencies in setup.cfg
-conda run -n base pip install -e $SCRIPT_DIR
+conda run -n base pip install -e "${$TAGBIO_PY}"
+#pip install -e "${TAGBIO_PY}"
 
 apt-get clean -y
 apt-get autoremove -y
