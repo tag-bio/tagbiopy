0f011d046ec3ec8b2dcd5124a44fd6a1fe0a142a
D <damir@tag.bio>
2023-06-28 13:38:31 -0700

    Install only tagbiopy with pip

--- install-package.sh
+++ install-package.sh
@@ -9,7 +9,6 @@ echo "*** Building tagbio python library in dev mode in ${TAGBIO_PY}"
 # Tue Jun 27 11:08:42 PDT 2023
 # To use tagbiopy as a package under pip, no dependencies in setup.cfg
 conda run -n base pip install -e "${TAGBIO_PY}"
-#pip install -e "${TAGBIO_PY}"
 
 apt-get clean -y
 apt-get autoremove -y
