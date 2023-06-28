64b19d826e257da34961e158589b3853b66dc8c7
D <damir@tag.bio>
2023-06-28 13:54:59 -0700

    Clean up install-package.sh

--- install-package.sh
+++ install-package.sh
@@ -4,10 +4,7 @@ set -ex
 
 echo "*** Building tagbio python library in dev mode in ${TAGBIO_PY}"
 
-#conda env update -f ${SCRIPT_DIR}/environment.yml
-
-# Tue Jun 27 11:08:42 PDT 2023
-# To use tagbiopy as a package under pip, no dependencies in setup.cfg
+# Use pip to install this SDK only. For dependencies use conda install
 conda run -n base pip install -e "${TAGBIO_PY}"
 
 apt-get clean -y
