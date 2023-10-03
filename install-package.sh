3cf7122a58a806dc2853ce0e7f290ac1c8625465
Wade Webster <wade@tag.bio>
2023-10-03 15:54:52 -0700

    install-package.sh -removed apt actions and switch to mamba

--- install-package.sh
+++ install-package.sh
@@ -5,7 +5,4 @@ set -ex
 echo "*** Building tagbio python library in dev mode in ${TAGBIO_PY}"
 
 # Use pip to install this SDK only. For dependencies use conda install
-conda run -n base pip install -e "${TAGBIO_PY}"
-
-apt-get clean -y
-apt-get autoremove -y
+mamba run -n base pip install -e "${TAGBIO_PY}"
